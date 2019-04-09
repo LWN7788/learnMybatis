e35c7a369876114d607da1ce257c5840c2ac063c
liujinyuan <liujinyuan@sunvua.com>
2019-04-09 17:17:35 +0800

    first

---
+++ src/me/gacl/domain/User.java
@@ -0,0 +1,27 @@
+package me.gacl.domain;
+
+public class User {
+    private int id;
+    private String title;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+    return "User [id=" + id + ", name=" + title + "]";
+    }
+}
